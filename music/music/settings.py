219ea50286dac3781f183ee1a1b1d060d094ded2
Abu Bakarr Bangurah <animevideogamer@gmail.com>
2020-12-06 23:58:09 -0500

    Revert "Revert "changes to watch later""
    This reverts commit 899bd6651f8a647233f4585eda1eebc107739864.

--- music/music/settings.py
+++ music/music/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = 'music.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'csi3450',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'USER': 'root',
-#         'PASSWORD': '903322mileRD!',
-#     }
-# }
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
+#}
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'csi3450',
+         'HOST': '127.0.0.1',
+         'PORT': '3306',
+         'USER': 'root',
+         'PASSWORD': '903322mileRD!',
+     }
+ }
 
 
 # Password validation
