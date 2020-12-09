49d2e432cc84bf6eb5d4fe0a980a476e72f5e5ae
Abu Bakarr Bangurah <animevideogamer@gmail.com>
2020-12-09 16:28:49 -0500

    Another Another Another Update

--- music/music/settings.py
+++ music/music/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = 'music.wsgi.application'
 
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'csi3450',
-         'HOST': '127.0.0.1',
-         'PORT': '3306',
-         'USER': 'root',
-         'PASSWORD': 'GamerGirl2!',
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+ #    'default': {
+ #        'ENGINE': 'django.db.backends.mysql',
+ #        'NAME': 'csi3450',
+ #        'HOST': '127.0.0.1',
+ #        'PORT': '3306',
+ #        'USER': 'root',
+ #        'PASSWORD': 'GamerGirl2!',
+ #    }
+# }
 
 
 # Password validation
